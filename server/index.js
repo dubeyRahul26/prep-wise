c1627765f5ad2438ff824762e7ecd428a165b259
dubeyRahul26 <153608907+dubeyRahul26@users.noreply.github.com>
2025-07-11 10:25:22 +0530

    Update

--- server/index.js
+++ server/index.js
@@ -42,7 +42,7 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../client/dist")));
 
 
-  app.get("/*", (req, res) => {
+  app.get("/{*splat}", (req, res) => {
     res.sendFile(path.join(__dirname, "../client/dist/index.html"));
   });
 }
